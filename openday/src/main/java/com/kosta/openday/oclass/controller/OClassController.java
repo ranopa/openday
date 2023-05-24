7bc3944691b2aaf1d7d5351572447fa812587a49
kmkim2051 <qaws2051@gmail.com>
2023-05-24 12:33:55 +0900

    각 메서드에 구분을 위한 주석 추가

--- openday/src/main/java/com/kosta/openday/oclass/controller/OClassController.java
+++ openday/src/main/java/com/kosta/openday/oclass/controller/OClassController.java
@@ -8,12 +8,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class OClassController {
 	
+	/**
+	 *  클래스 상세 화면에서, "신청하기" 버튼 클릭 시
+	 * */
 	@RequestMapping(value="/applyclass")
 	public String applyClass() {
 		return "applyClass";
 	}
+	
+	/**
+	 * 클래스 신청 화면에서, "다음으로" 버튼 클릭 시
+	 * */
 	@RequestMapping(value="/paymentprocess")
 	public String paymentProcess() {
 		return "paymentProcess";
+	}
+	
+	/**
+	 * 클래스 결제정보 확인 화면에서, "결제하기 버튼 클릭 시
+	 * */
+	@RequestMapping(value="/payment", method=RequestMethod.POST)
+	public String payment() {
+		return "paymentResult";
 	}
 }
