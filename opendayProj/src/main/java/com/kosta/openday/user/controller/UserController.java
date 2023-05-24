a0f75e825910d68fb1cabc70c08712d7c68c6953
binunu <87841872+binunu@users.noreply.github.com>
2023-05-24 13:32:22 +0900

    ...

--- opendayProj/src/main/java/com/kosta/openday/user/controller/UserController.java
+++ opendayProj/src/main/java/com/kosta/openday/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.kosta.openday.user.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kosta.openday.user.dto.UserDTO;
 import com.kosta.openday.user.service.UserService;
@@ -14,26 +17,46 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private HttpSession session;
 
 	@RequestMapping("/")
 	public String main() {
 		return "main";
 	}
 
+	//회원가입폼
 	@RequestMapping("/joinform")
 	public String joinForm() {
-		return "join";
+		return "mypage/join";
 	}
 
+	//회원가입
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
 	public String joinUser(@ModelAttribute UserDTO user) throws Exception {
 		try { 
-			System.out.println("success");
-			userService.JoinUser(user);
+			userService.JoinUser(user); 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "joinviewtest";
+		return "mypage/joinResult";
 	}
+
+	
+	//마이페이지 > 태희언니 헤더 가져오면 지움(테스트용) 
+	@RequestMapping(value = "/mypage", method=RequestMethod.GET)
+	public ModelAndView myPayge() {
+		ModelAndView mav = new ModelAndView();
+		try {
+			String id = "sbsb";
+			session.setAttribute("id", id); 
+			mav.setViewName("mypage/myPage");
+		}catch(Exception e) {
+			System.out.println("실패");
+			e.printStackTrace();
+		}
+		return mav;
+	}
+	
 
 }
