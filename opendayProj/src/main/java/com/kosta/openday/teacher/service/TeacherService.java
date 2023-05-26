22d292151cd35cc64e602f214151e8e8d6745ab6
kec0486 <dmschddlrnsk@naver.com>
2023-05-26 11:26:15 +0900

    2023 05 26 am 11:26

--- opendayProj/src/main/java/com/kosta/openday/teacher/service/TeacherService.java
+++ opendayProj/src/main/java/com/kosta/openday/teacher/service/TeacherService.java
@@ -1,5 +1,9 @@
 package com.kosta.openday.teacher.service;
 
+import java.util.List;
+
+import com.kosta.openday.adm.dto.AnnouncementDTO;
+
 public interface TeacherService {
-	
+	List<AnnouncementDTO> tcAnnouncement() throws Exception;
 }
--- opendayProj/src/main/java/com/kosta/openday/teacher/service/TeacherServiceImpl.java
+++ opendayProj/src/main/java/com/kosta/openday/teacher/service/TeacherServiceImpl.java
@@ -1,8 +1,20 @@
 package com.kosta.openday.teacher.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kosta.openday.adm.dto.AnnouncementDTO;
+import com.kosta.openday.teacher.dao.TeacherDAO;
 
 @Service
 public class TeacherServiceImpl implements TeacherService {
+	@Autowired
+	private TeacherDAO teacherDAO;
+	@Override
+	public List<AnnouncementDTO> tcAnnouncement() throws Exception {
+		return teacherDAO.tcAnnouncement();
+	}
 
 }
